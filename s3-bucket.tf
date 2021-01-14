95139af4e667022cac6af8ea5c2f58a560c604bd
Sergio <you@example.com>
2021-01-14 23:45:40 +0000

    Added s3 bucket mod

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,5 +2,8 @@ module "s3-bucket_example_complete" {
   source = "terraform-aws-modules/s3-bucket/aws"
 
   bucket_prefix = "serazo"
-  tags = "testing"
+  tags = [
+      "Department" = "IT"
+      "Billable" = "Yes"
+  ]
 }
